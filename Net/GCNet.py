635b0ce1deed808cbbad393c1ab0b99b441f67ee
pandaominggz <pandaominggz@qq.com>
2020-02-06 00:47:34 +0000

    section 3 and 4 commit

--- Net/GCNet.py
+++ Net/GCNet.py
@@ -15,9 +15,9 @@ class GCNet(nn.Module):
     def build_module(self):
         self.layer_dict = nn.ModuleDict()
         section1 = SectionOne(self.height, self.width, self.channels)
-        # print(section1)
         section2 = SectionTwo(self.height, self.width, self.channels)
-        # print(section2)
+        section3 = SectionThree(self.height, self.width, self.channels)
+        section4 = SectionFour(self.height, self.width, self.channels)
 
     def forward(self, x):
         return x
@@ -37,75 +37,274 @@ class SectionOne(nn.Module):
 
     def build_module(self):
         self.layer_dict = nn.ModuleDict()
-        x = torch.zeros((self.height, self.width))
 
-        self.layer_dict['conv_0'] = nn.Conv2d(self.channels, self.features, (5, 5), 2, 2)
-        x = self.layer_dict['conv_0'].forward(x)
+        self.layer_dict['conv2d_0'] = nn.Conv2d(self.channels, self.features, (5, 5), 2, 2)
+        self.layer_dict['bn2d_0'] = nn.BatchNorm2d(32)
+        self.layer_dict['relu2d_0'] = nn.ReLU()
+        self.resNetBlock()
+        self.layer_dict['conv2d_1'] = nn.Conv2d(self.features, self.features, (3, 3), 1, 1)
 
-        self.layer_dict['bn_0'] = nn.BatchNorm2d(32)
-        x = self.layer_dict['bn_0'].forward(x)
-
-        self.layer_dict['relu_0'] = nn.ReLU()
-        x = self.layer_dict['relu_0'].forward(x)
-
-        x = self.resNetBlock(self.num_resBlock, x)
-
-        self.layer_dict['conv_1'] = nn.Conv2d(self.features, self.features, (3, 3), 1, 1)
-        x = self.layer_dict['conv_1'].forward(x)
-
-        return x
-
-    def resNetBlock(self, input):
-        output = input
+    def resNetBlock(self):
         for i in range(self.num_resBlock * 2):
-
-            self.layer_dict['res_conv_' + i] = nn.Conv2d(self.features, self.features, (3, 3), 1, 1)
-            output = self.layer_dict['res_conv_' + i].forward(output)
-
-            self.layer_dict['res_bn_' + i] = nn.BatchNorm2d(32)
-            output = self.layer_dict['res_bn_' + i].forward(output)
+            self.layer_dict['res_conv2d_' + i] = nn.Conv2d(self.features, self.features, (3, 3), 1, 1)
+            self.layer_dict['res_bn2d_' + i] = nn.BatchNorm2d(32)
 
             if i % 2 == 0:
-                self.layer_dict['res_relu_' + i] = nn.ReLU()
-                output = self.layer_dict['res_relu_' + i].forward(output)
-
+                self.layer_dict['res_relu2d_' + i] = nn.ReLU()
             else:
-                output = output + input
-                self.layer_dict['res_relu_' + i] = nn.ReLU()
-                output = self.layer_dict['res_relu_' + i].forward(output)
-                input = output
+                self.layer_dict['res_relu2d_' + i] = nn.ReLU()
 
-        return output
+    def forward(self, imgl, imgr):
+        imgl = self.layer_dict['conv2d_0'].forward(imgl)
+        imgr = self.layer_dict['conv2d_0'].forward(imgr)
+        imgl = self.layer_dict['bn2d_0'].forward(imgl)
+        imgr = self.layer_dict['bn2d_0'].forward(imgr)
+        imgl = self.layer_dict['relu2d_0'].forward(imgl)
+        imgr = self.layer_dict['relu2d_0'].forward(imgr)
 
-    def forward(self, x):
-        x = self.layer_dict['conv_0'].forward(x)
-        x = self.layer_dict['bn_0'].forward(x)
-        x = self.layer_dict['relu_0'].forward(x)
-        output = x
+        outputl, outputr = imgl, imgr
         for i in range(self.num_resBlock * 2):
-            output = self.layer_dict['res_conv_' + i].forward(output)
-            output = self.layer_dict['res_bn_' + i].forward(output)
-            if i % 2 == 0:
-                output = self.layer_dict['res_relu_' + i].forward(output)
-            else:
-                output = output + x
-                output = self.layer_dict['res_relu_' + i].forward(output)
-                x = output
+            outputl = self.layer_dict['res_conv2d_' + i].forward(outputl)
+            outputr = self.layer_dict['res_conv2d_' + i].forward(outputr)
+            outputl = self.layer_dict['res_bn2d_' + i].forward(outputl)
+            outputr = self.layer_dict['res_bn2d_' + i].forward(outputr)
 
-        output = self.layer_dict['conv_1'].forward(output)
-        return output
+            if i % 2 == 0:
+                outputl = self.layer_dict['res_relu2d_' + i].forward(outputl)
+                outputr = self.layer_dict['res_relu2d_' + i].forward(outputr)
+            else:
+                outputl = outputl + imgl
+                outputr = outputl + imgr
+                outputl = self.layer_dict['res_relu2d_' + i].forward(outputl)
+                outputr = self.layer_dict['res_relu2d_' + i].forward(outputr)
+                imgl = outputl
+                imgr = outputr
+
+        outputl = self.layer_dict['conv2d_1'].forward(outputl)
+        outputr = self.layer_dict['conv2d_1'].forward(outputr)
+        return outputl, outputr
 
 
 class SectionTwo(nn.Module):
-    def __init__(self):
+    def __init__(self, maxdisp):
         super(SectionTwo, self).__init__()
         # initial something
+        self.maxdisp = maxdisp
+
+    def cost_volume(self, imgl, imgr):
+        xx_list = []
+        pad_opr1 = nn.ZeroPad2d((0, self.maxdisp, 0, 0))
+        xleft = pad_opr1(imgl)
+        for d in range(self.maxdisp):
+            pad_opr2 = nn.ZeroPad2d((d, self.maxdisp - d, 0, 0))
+            xright = pad_opr2(imgr)
+            xx_temp = torch.cat((xleft, xright), 1)
+            xx_list.append(xx_temp)
+        xx = torch.cat(xx_list, 1)
+        xx = xx.view(1, self.maxdisp, 64, int(self.height / 2), int(self.width / 2) + self.maxdisp)
+        xx0 = xx.permute(0, 2, 1, 3, 4)
+        xx0 = xx0[:, :, :, :, :int(self.width / 2)]
+        return xx0
+
+    def forward(self, imgl, imgr):
+        x = self.cost_volume(imgl, imgr)
+        return x
+
+
+class SectionThree(nn.Module):
+    def __init__(self, input):
+        super(SectionThree, self).__init__()
+        # initial something
+        self.input = input
         self.build_module()
 
     def build_module(self):
         self.layer_dict = nn.ModuleDict()
-        x = torch.zeros((self.height, self.width))
-        return x
 
-    def forward(self, x):
-        return 0
+        self.layer_dict['conv3d_0'] = nn.Conv3d(64, 32, (3, 3, 3), 1, 1)
+        self.layer_dict['bn3d_0'] = nn.BatchNorm3d(32)
+        self.layer_dict['relu3d_0'] = nn.ReLU()
+
+        self.layer_dict['conv3d_1'] = nn.Conv3d(32, 32, (3, 3, 3), 1, 1)
+        self.layer_dict['bn3d_1'] = nn.BatchNorm3d(32)
+        self.layer_dict['relu3d_1'] = nn.ReLU()
+
+        # 1
+        self.layer_dict['conv3d_2'] = nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=1)
+        self.layer_dict['bn3d_2'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_2'] = nn.ReLU()
+
+        self.layer_dict['conv3d_3'] = nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_3'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_3'] = nn.ReLU()
+
+        self.layer_dict['conv3d_4'] = nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_4'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_4'] = nn.ReLU()
+
+        # 2
+        self.layer_dict['conv3d_5'] = nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=1)
+        self.layer_dict['bn3d_5'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_5'] = nn.ReLU()
+
+        self.layer_dict['conv3d_6'] = nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_6'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_6'] = nn.ReLU()
+
+        self.layer_dict['conv3d_7'] = nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_7'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_7'] = nn.ReLU()
+
+        # 3
+        self.layer_dict['conv3d_8'] = nn.Conv3d(64, 64, kernel_size=3, stride=2, padding=1)
+        self.layer_dict['bn3d_8'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_8'] = nn.ReLU()
+
+        self.layer_dict['conv3d_9'] = nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_9'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_9'] = nn.ReLU()
+
+        self.layer_dict['conv3d_10'] = nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_10'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_10'] = nn.ReLU()
+
+        # 4
+        self.layer_dict['conv3d_11'] = nn.Conv3d(64, 128, kernel_size=3, stride=2, padding=1)
+        self.layer_dict['bn3d_11'] = nn.BatchNorm3d(128)
+        self.layer_dict['relu3d_11'] = nn.ReLU()
+
+        self.layer_dict['conv3d_12'] = nn.Conv3d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_12'] = nn.BatchNorm3d(128)
+        self.layer_dict['relu3d_12'] = nn.ReLU()
+
+        self.layer_dict['conv3d_13'] = nn.Conv3d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.layer_dict['bn3d_13'] = nn.BatchNorm3d(128)
+        self.layer_dict['relu3d_13'] = nn.ReLU()
+
+        self.layer_dict['Tconv3d_0'] = nn.ConvTranspose3d(128, 64, 3, 2, 1, 1)
+        self.layer_dict['Tbn3d_0'] = nn.BatchNorm3d(64)
+        self.layer_dict['Trelu3d_0'] = nn.ReLU()
+
+        self.layer_dict['Tconv3d_1'] = nn.ConvTranspose3d(64, 64, 3, 2, 1, 1)
+        self.layer_dict['Tbn3d_1'] = nn.BatchNorm3d(64)
+        self.layer_dict['Trelu3d_1'] = nn.ReLU()
+
+        self.layer_dict['Tconv3d_2'] = nn.ConvTranspose3d(64, 64, 3, 2, 1, 1)
+        self.layer_dict['Tbn3d_2'] = nn.BatchNorm3d(64)
+        self.layer_dict['Trelu3d_2'] = nn.ReLU()
+
+        self.layer_dict['Tconv3d_3'] = nn.ConvTranspose3d(64, 32, 3, 2, 1, 1)
+        self.layer_dict['Tbn3d_3'] = nn.BatchNorm3d(32)
+        self.layer_dict['Trelu3d_3'] = nn.ReLU()
+
+        self.layer_dict['Tconv3d_4'] = nn.ConvTranspose3d(32, 1, 3, 2, 1, 1)
+
+        self.layer_dict['conv3d_14'] = nn.Conv3d(64, 64, 3, 2, 1)
+        self.layer_dict['bn3d_14'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_14'] = nn.ReLU()
+        self.layer_dict['conv3d_15'] = nn.Conv3d(64, 64, 3, 2, 1)
+        self.layer_dict['bn3d_15'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_15'] = nn.ReLU()
+        self.layer_dict['conv3d_16'] = nn.Conv3d(64, 64, 3, 2, 1)
+        self.layer_dict['bn3d_16'] = nn.BatchNorm3d(64)
+        self.layer_dict['relu3d_16'] = nn.ReLU()
+
+    def forward(self, cost_volum):
+        x = cost_volum
+        x = self.layer_dict['conv3d_0'].forward(x)
+        x = self.layer_dict['bn3d_0'].forward(x)
+        x = self.layer_dict['relu3d_0'].forward(x)
+        x = self.layer_dict['conv3d_1'].forward(x)
+        x = self.layer_dict['bn3d_1'].forward(x)
+        conv3d_20 = self.layer_dict['relu3d_1'].forward(x)
+
+        conv3d_block_1 = self.layer_dict['conv3d_2'].forward(cost_volum)
+        conv3d_block_1 = self.layer_dict['bn3d_2'].forward(conv3d_block_1)
+        conv3d_block_1 = self.layer_dict['relu3d_2'].forward(conv3d_block_1)
+        conv3d_block_1 = self.layer_dict['conv3d_3'].forward(conv3d_block_1)
+        conv3d_block_1 = self.layer_dict['bn3d_3'].forward(conv3d_block_1)
+        conv3d_block_1 = self.layer_dict['relu3d_3'].forward(conv3d_block_1)
+        conv3d_block_1 = self.layer_dict['conv3d_4'].forward(conv3d_block_1)
+        conv3d_block_1 = self.layer_dict['bn3d_4'].forward(conv3d_block_1)
+        conv3d_block_1 = self.layer_dict['relu3d_4'].forward(conv3d_block_1)
+
+        # conv3d block
+        conv3d_21 = self.layer_dict['conv3d_14'].forward(cost_volum)
+        conv3d_21 = self.layer_dict['bn3d_14'].forward(conv3d_21)
+        conv3d_21 = self.layer_dict['relu3d_14'].forward(conv3d_21)
+
+        conv3d_block_2 = self.layer_dict['conv3d_5'].forward(conv3d_21)
+        conv3d_block_2 = self.layer_dict['bn3d_5'].forward(conv3d_block_2)
+        conv3d_block_2 = self.layer_dict['relu3d_5'].forward(conv3d_block_2)
+        conv3d_block_2 = self.layer_dict['conv3d_6'].forward(conv3d_block_2)
+        conv3d_block_2 = self.layer_dict['bn3d_6'].forward(conv3d_block_2)
+        conv3d_block_2 = self.layer_dict['relu3d_6'].forward(conv3d_block_2)
+        conv3d_block_2 = self.layer_dict['conv3d_7'].forward(conv3d_block_2)
+        conv3d_block_2 = self.layer_dict['bn3d_7'].forward(conv3d_block_2)
+        conv3d_block_2 = self.layer_dict['relu3d_7'].forward(conv3d_block_2)
+
+        conv3d_24 = self.layer_dict['conv3d_15'].forward(conv3d_21)
+        conv3d_24 = self.layer_dict['bn3d_15'].forward(conv3d_24)
+        conv3d_24 = self.layer_dict['relu3d_15'].forward(conv3d_24)
+
+        conv3d_block_3 = self.layer_dict['conv3d_8'].forward(conv3d_24)
+        conv3d_block_3 = self.layer_dict['bn3d_8'].forward(conv3d_block_3)
+        conv3d_block_3 = self.layer_dict['relu3d_8'].forward(conv3d_block_3)
+        conv3d_block_3 = self.layer_dict['conv3d_9'].forward(conv3d_block_3)
+        conv3d_block_3 = self.layer_dict['bn3d_9'].forward(conv3d_block_3)
+        conv3d_block_3 = self.layer_dict['relu3d_9'].forward(conv3d_block_3)
+        conv3d_block_3 = self.layer_dict['conv3d_10'].forward(conv3d_block_3)
+        conv3d_block_3 = self.layer_dict['bn3d_10'].forward(conv3d_block_3)
+        conv3d_block_3 = self.layer_dict['relu3d_10'].forward(conv3d_block_3)
+
+        conv3d_27 = self.layer_dict['conv3d_15'].forward(conv3d_24)
+        conv3d_27 = self.layer_dict['bn3d_15'].forward(conv3d_27)
+        conv3d_27 = self.layer_dict['relu3d_15'].forward(conv3d_27)
+
+        conv3d_block_4 = self.layer_dict['conv3d_11'].forward(conv3d_27)
+        conv3d_block_4 = self.layer_dict['bn3d_11'].forward(conv3d_block_4)
+        conv3d_block_4 = self.layer_dict['relu3d_11'].forward(conv3d_block_4)
+        conv3d_block_4 = self.layer_dict['conv3d_12'].forward(conv3d_block_4)
+        conv3d_block_4 = self.layer_dict['bn3d_12'].forward(conv3d_block_4)
+        conv3d_block_4 = self.layer_dict['relu3d_12'].forward(conv3d_block_4)
+        conv3d_block_4 = self.layer_dict['conv3d_13'].forward(conv3d_block_4)
+        conv3d_block_4 = self.layer_dict['bn3d_13'].forward(conv3d_block_4)
+        conv3d_block_4 = self.layer_dict['relu3d_13'].forward(conv3d_block_4)
+
+        # deconv
+        deconv3d = self.layer_dict['Tconv3d_0'].forward(conv3d_block_4)
+        deconv3d = self.layer_dict['Tbn3d_0'].forward(deconv3d + conv3d_block_3)
+        deconv3d = self.layer_dict['Trelu3d_0'].forward(deconv3d)
+
+        deconv3d = self.layer_dict['Tconv3d_1'].forward(deconv3d)
+        deconv3d = self.layer_dict['Tbn3d_1'].forward(deconv3d + conv3d_block_2)
+        deconv3d = self.layer_dict['Trelu3d_1'].forward(deconv3d)
+
+        deconv3d = self.layer_dict['Tconv3d_2'].forward(deconv3d)
+        deconv3d = self.layer_dict['Tbn3d_2'].forward(deconv3d + conv3d_block_1)
+        deconv3d = self.layer_dict['Trelu3d_2'].forward(deconv3d)
+
+        deconv3d = self.layer_dict['Tconv3d_3'].forward(deconv3d)
+        deconv3d = self.layer_dict['Tbn3d_3'].forward(deconv3d + conv3d_20)
+        deconv3d = self.layer_dict['Trelu3d_3'].forward(deconv3d)
+
+        deconv3d = self.layer_dict['Trelu3d_4'].forward(deconv3d)
+        out = deconv3d.view(1, self.maxdisp * 2, self.height, self.width)
+
+        return out
+
+
+class SectionFour(nn.Module):
+    def __init__(self, input):
+        super(SectionFour, self).__init__()
+        # initial something
+        self.input = input
+        self.build_module()
+
+        def build_module(self):
+            self.layer_dict = nn.ModuleDict()
+            self.layer_dict['softMax'] = nn.Softmax()
+
+    def forward(self, input):
+        x = self.layer_dict['softMax'].forward(-input, 1)
+        return x
